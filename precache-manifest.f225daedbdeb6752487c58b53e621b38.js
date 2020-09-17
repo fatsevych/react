a213e55f1edbb3e35ff830155cd64deb944dbae0
Anna Fatsevych <anna.fatsevych@flightnetwork.com>
2020-09-17 14:53:51 -0400

    Updates

---
+++ precache-manifest.f225daedbdeb6752487c58b53e621b38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5505449715a64d35a5667d00581d1224",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "6dbcd94a886a337879db",
+    "url": "/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0babbde14e333bc689a2",
+    "url": "/react-app/static/js/2.8aaac6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.8aaac6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dbcd94a886a337879db",
+    "url": "/react-app/static/js/main.d1a6e05f.chunk.js"
+  },
+  {
+    "revision": "e2c6b0a16d423a796dce",
+    "url": "/react-app/static/js/runtime-main.9a102076.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
